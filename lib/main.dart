3e0ba1ec8fbeae255f82ec6756029f6965a4d5c3
David Chen <david@wearise.com>
2023-07-30 22:56:14 -0500

    add bg color

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          body: Container(
+        color: const Color.fromARGB(255, 64, 4, 124),
+      )),
+    ),
+  );
+}
