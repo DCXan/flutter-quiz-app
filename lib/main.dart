42f3f165e10cd7800f2139b5f7aea0156d5babdd
David Chen <david@wearise.com>
2023-08-10 16:21:14 -0500

    add asset and button

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+void buttonPress() {
+  debugPrint("button pressed");
+}
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-          body: Container(
-        color: const Color.fromARGB(255, 64, 4, 124),
-      )),
+        body: Container(
+          color: const Color.fromARGB(255, 64, 4, 124),
+          child: Column(
+            children: [
+              Image.asset(
+                '/Users/davidchen/Desktop/Flutter-Projects/flutter-quiz-app/quiz_app/assets/images/quiz-logo.png',
+              ),
+              const TextButton(onPressed: buttonPress, child: Text('Start Quiz'))
+            ],
+          ),
+        ),
+      ),
     ),
   );
 }
