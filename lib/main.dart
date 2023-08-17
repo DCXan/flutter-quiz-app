112725f88f1fd352deed8c199b235848ad703cba
David Chen <chen.david.1016@gmail.com>
2023-08-17 15:49:35 -0500

    refactor screen to custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,18 @@
-
 import 'package:flutter/material.dart';
-
-void buttonPress() {
-  debugPrint("button pressed");
-}
+import 'start_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         body: Container(
-          color: const Color.fromARGB(255, 64, 4, 124),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                // '/Users/davidchen/Desktop/Flutter-Projects/flutter-quiz-app/quiz_app/assets/images/quiz-logo.png',
-                'assets/images/quiz-logo.png',
-              ),
-              const Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 24,
-                ),
-              ),
-              ElevatedButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 64, 4, 124),
-                ),
-                onPressed: buttonPress,
-                child: const Text('Start Quiz'),
-              )
-            ],
-          ),
-        ),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color.fromARGB(255, 110, 8, 128),
+                Color.fromARGB(255, 79, 7, 92),
+              ]),
+            ),
+            child: const StartScreen()),
       ),
     ),
   );
